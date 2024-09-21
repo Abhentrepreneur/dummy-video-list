c25b43f7704bce03289c0b9e235547276d3ecdb9
Abhentrepreneur <117310354+Abhentrepreneur@users.noreply.github.com>
2024-09-21 21:37:04 +0530

    Update authService.ts

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://dummy-video-list-jkp9.vercel.app/';
+const API_URL = 'https://dummy-video-list-jkp9.vercel.app/api/auth';
 
 export const signup = (email: string, password: string) => {
   return axios.post(`${API_URL}/signup`, { email, password });
