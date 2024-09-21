31172daf596f8834d91aa7bff08ede4105ef45fb
Abhentrepreneur <117310354+Abhentrepreneur@users.noreply.github.com>
2024-09-21 21:32:17 +0530

    Update videoService.ts

--- frontend/src/services/videoService.ts
+++ frontend/src/services/videoService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'dummy-video-list-jkp9.vercel.app/';
+const API_URL = 'https://dummy-video-list-jkp9.vercel.app/';
 
 export const getVideos = () => {
   return axios.get(`${API_URL}`);
