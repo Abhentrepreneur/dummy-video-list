d6ff15bab65990e7978baa672cf9aaf799377487
Abhentrepreneur <117310354+Abhentrepreneur@users.noreply.github.com>
2024-09-22 15:25:34 +0530

    Update videoService.ts

--- frontend/src/services/videoService.ts
+++ frontend/src/services/videoService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/videos';
+const API_URL = 'https://00bd-165-225-124-166.ngrok-free.app/api/videos';
 
 export const getVideos = () => {
   return axios.get(`${API_URL}`);
