305b1a1ad2314ac8a896451d1cae67d10c402bb4
Abhentrepreneur <117310354+Abhentrepreneur@users.noreply.github.com>
2024-09-23 23:51:59 +0530

    Update authService.ts

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://00bd-165-225-124-166.ngrok-free.app/api/auth';
+const API_URL = 'https://c2b7-165-225-124-155.ngrok-free.app/api/auth';
 
 export const signup = (email: string, password: string) => {
   return axios.post(`${API_URL}/signup`, { email, password });
